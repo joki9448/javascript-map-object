7ba171997fefe70c35cddbb0e64af841254b206f
John Kim <joki9448@gmail.com>
2022-12-17 15:34:12 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const myArray = [1, 2, 3, 4, 5, 5, 5, 1, 2];
+const mySet = new Set(myArray);
+
+// const myMap = new Map([ ['name', 'John'], ['surname', 'Doe']])
+// console.log(myMap)
+
+// const myObject = {};
+
+// const a = {};
+// const b = {};
+
+// myObject[a] = 'a';
+// myObject[a] = 'b';
+
+// console.log(myObject)
+
+// const a = {};
+// const b = {};
+
+// const myMap = new Map([ [a, 'a'], [b, 'b'] ])
+// console.log(myMap)
+
+
+// const myMap = new Map([ [{}, 'a'], [{}, 'b'] ])
+
+// myMap.set('a', 2);
+
+// console.log(myMap);
+
+
+// const myMap = new Map([ [1, 'one'] ])
+// myMap.delete('one');
+// console.log(myMap)
+
+
+// console.log(myArray)
+// console.log(mySet)
+
+// const uniqueArray = [...mySet];
+
+// console.log(uniqueArray);
+
+// mySet.add(6);
+// mySet.add('6');
+// mySet.add({ name: 'John' })
+
+// mySet.delete(3);
+
+// mySet.clear();
+// console.log(mySet)
+
+// console.log(mySet.has(6));
+// console.log(mySet.size);
+
